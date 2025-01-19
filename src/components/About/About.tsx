0423a16190dd1cc8e86d2c522f4d688ab662228d
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-01-19 15:31:32 -0500

    About is migrated

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,56 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+import laptopImg from "../../Assets/about.png";
+
+import Particle from "../Particle";
+import Techstack from "./Techstack";
+import Aboutcard from "./AboutCard";
+import Toolstack from "./Toolstack";
 
 function About() {
     return (
         <Container fluid className="about-section">
-            About
+            <Particle />
+            <Container>
+                <Row style={{ justifyContent: "center", padding: "10px" }}>
+                    <Col
+                        md={7}
+                        style={{
+                            justifyContent: "center",
+                            paddingTop: "30px",
+                            paddingBottom: "50px",
+                        }}
+                    >
+                        <h1
+                            style={{ fontSize: "2.1em", paddingBottom: "20px" }}
+                        >
+                            Know Who <strong className="purple">I'M</strong>
+                        </h1>
+                        <Aboutcard />
+                    </Col>
+                    <Col
+                        md={5}
+                        style={{ paddingTop: "120px", paddingBottom: "50px" }}
+                        className="about-img"
+                    >
+                        <img
+                            src={laptopImg}
+                            alt="about"
+                            className="img-fluid"
+                        />
+                    </Col>
+                </Row>
+                <h1 className="project-heading">
+                    Professional <strong className="purple">Skillset </strong>
+                </h1>
+
+                <Techstack />
+
+                <h1 className="project-heading">
+                    <strong className="purple">Tools</strong> I use
+                </h1>
+                <Toolstack />
+            </Container>
         </Container>
     );
 }
