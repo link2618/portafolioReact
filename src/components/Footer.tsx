5738fc53931182faf009e48a36ce7e36dff7f76b
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-02-05 16:08:21 -0500

    Added basic SEO

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,16 +54,6 @@ function Footer() {
                                 <FaLinkedinIn />
                             </a>
                         </li>
-                        {/* <li className="social-icons">
-                            <a
-                                href="https://www.instagram.com/soumyajit4419"
-                                style={{ color: "white" }}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <AiFillInstagram />
-                            </a>
-                        </li> */}
                     </ul>
                 </Col>
             </Row>
